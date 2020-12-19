7d54a03b46be64f417de705231398698b158e53d
omark <o.markmann@gmail.com>
2020-12-19 16:49:42 +0100

    update angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {NumberGeneratorComponent} from './components/number-generator/number-generator.component';
 import {PlayerStatsComponent} from './components/player-stats/player-stats.component';
 import {PlayerStatsDetailComponent} from './components/player-stats-detail/player-stats-detail.component'; // CLI imports router
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
